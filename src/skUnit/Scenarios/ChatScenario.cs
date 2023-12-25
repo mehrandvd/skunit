b747569b933f0cb683bad60f3c0dd3f2d5d52f4e
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-12-26 00:35:44 +0330

    Add xml doc fo scenarios

--- src/skUnit/Scenarios/ChatScenario.cs
+++ src/skUnit/Scenarios/ChatScenario.cs
@@ -3,10 +3,20 @@ using skUnit.Scenarios.Parsers.Assertions;
 
 namespace skUnit.Scenarios;
 
+/// <summary>
+/// An InvokeScenario is a scenario to test a ChatCompletionService, Kernel, Function or Plugin.
+/// It contains the required inputs to call a InvokeAsync like: ChatItems.
+/// Also it contains the expected output for each level of chat by: Assertions
+/// </summary>
 public class ChatScenario
 {
     public string? Description { get; set; }
     public required string RawText { get; set; }
+
+    /// <summary>
+    /// The ChatItems that should be applied to chat history one by one and
+    /// check for assertions for each level.
+    /// </summary>
     public List<ChatItem> ChatItems { get; set; } = new();
 
 }
@@ -20,6 +30,10 @@ public class ChatItem
     }
     public AuthorRole Role { get; set; }
     public string Content { get; set; }
+
+    /// <summary>
+    /// All the assertions that should be checked after the result of InvokeAsync is ready for the user input and history so far.
+    /// </summary>
     public List<IKernelAssertion> Assertions { get; set; } = new();
 
     public override string ToString()
--- src/skUnit/Scenarios/InvokeScenario.cs
+++ src/skUnit/Scenarios/InvokeScenario.cs
@@ -9,10 +9,32 @@ namespace skUnit.Scenarios;
 /// </summary>
 public class InvokeScenario
 {
+    /// <summary>
+    /// This property will only be used if this scenario is running on a kernel like:
+    /// <code>
+    /// await kernel.InvokePromptAsync(prompt, arguments)
+    /// </code>
+    /// So this prompt will NOT affect when testing functions. The functions just will get Parameters as their arguments.
+    /// </summary>
     public string? Prompt { get; set; }
+    
     public string? Description { get; set; }
+    
     public required string RawText { get; set; }
+    
+    /// <summary>
+    /// The parameters that will be passed as arguments while invoking by a kernel or function.
+    /// </summary>
     public Dictionary<string, string> Parameters { get; set; } = new();
+    
+    /// <summary>
+    /// All the assertions that should be checked after the result of InvokeAsync is ready.
+    /// </summary>
     public List<IKernelAssertion> Assertions { get; set; } = new();
+    
+    /// <summary>
+    /// The ExpectedAnswer that stated as ANSWER in the scenario. It will not be checked directly, unless there is
+    /// a specific assertion for it in Assertions.
+    /// </summary>
     public string? ExpectedAnswer { get; set; }
 }
