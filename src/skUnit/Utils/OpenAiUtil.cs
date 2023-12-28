18b29a65876de10f65e2f31d4cd60bfcbe8e7089
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-12-28 20:01:25 +0330

    Add xml doc for ParseJson

--- src/skUnit/Utils/OpenAiUtil.cs
+++ src/skUnit/Utils/OpenAiUtil.cs
@@ -11,6 +11,16 @@ namespace skUnit.Utils
 {
     public class OpenAiUtil
     {
+        /// <summary>
+        /// Parses <paramref name="text"/> to JSON in a more tolerant way suitable
+        /// for OpenAI results.
+        /// Sometimes even the prompt dictated to return a JSON, but OpenAI returns it
+        /// with some additional characters like a heading and tailing ```, or even a starting ```json.
+        /// This method tries its best to parse these texts to a JSON.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="text"></param>
+        /// <returns></returns>
         public static T? ParseJson<T>(string text)
         {
             var trim =
