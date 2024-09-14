25a7a4fccb4ed0b61d391e69448c470199e9eaea
Mehran Davoudi <mehran.davoudi@hotmail.com>
2024-09-15 01:42:42 +0330

    Decouple tests from kernels.

--- src/skUnit/Asserts/SemanticKernelAssert_Chat.cs
+++ src/skUnit/Asserts/SemanticKernelAssert_Chat.cs
@@ -11,6 +11,96 @@ namespace skUnit;
 public partial class SemanticKernelAssert
 {
     /// <summary>
+    /// Checks whether the <paramref name="scenario"/> passes on the given <paramref name="getAnswerFunc"/>
+    /// using its ChatCompletionService.
+    /// </summary>
+    /// <param name="scenario"></param>
+    /// <param name="getAnswerFunc"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException">If the OpenAI was unable to generate a valid response.</exception>
+    public async Task CheckChatScenarioAsync(ChatScenario scenario, Func<ChatHistory, Task<string>> getAnswerFunc)
+    {
+        var chatHistory = new ChatHistory();
+
+        Log($"# SCENARIO {scenario.Description}");
+        Log("");
+
+        var queue = new Queue<ChatItem>(scenario.ChatItems);
+
+        while (queue.Count > 0)
+        {
+            var chatItem = queue.Dequeue();
+
+
+            if (chatItem.Role == AuthorRole.System)
+            {
+                chatHistory.AddSystemMessage(chatItem.Content);
+                Log($"## [{chatItem.Role.ToString().ToUpper()}]");
+                Log(chatItem.Content);
+                Log();
+            }
+            else if (chatItem.Role == AuthorRole.User)
+            {
+                chatHistory.AddUserMessage(chatItem.Content);
+                Log($"## [{chatItem.Role.ToString().ToUpper()}]");
+                Log(chatItem.Content);
+                Log();
+            }
+            else if (chatItem.Role == AuthorRole.Assistant)
+            {
+                Log($"## [EXPECTED ANSWER]");
+                Log(chatItem.Content);
+                Log();
+
+                var answer = await getAnswerFunc(chatHistory);
+
+                // To let chatHistory stay clean for getting the answer
+                chatHistory.AddAssistantMessage(chatItem.Content);
+
+
+                Log($"### [ACTUAL ANSWER]");
+                Log(answer);
+                Log();
+
+                foreach (var assertion in chatItem.Assertions)
+                {
+                    await CheckAssertionAsync(assertion, answer);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Checks whether all of the <paramref name="scenarios"/> passes on the given <paramref name="kernel"/>
+    /// using its ChatCompletionService.
+    /// If you want to test the kernel using something other than ChatCompletionService (for example using your own function),
+    /// pass <paramref name="getAnswerFunc"/> and specify how do you want the answer be created from chat history like:
+    /// <code>
+    /// getAnswerFunc = async history =>
+    ///     await AnswerChatFunction.InvokeAsync(kernel, new KernelArguments()
+    ///     {
+    ///         ["history"] = history,
+    ///     });
+    /// </code>
+    /// </summary>
+    /// <param name="kernel"></param>
+    /// <param name="scenarios"></param>
+    /// <param name="getAnswerFunc"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException">If the OpenAI was unable to generate a valid response.</exception>
+    public async Task CheckChatScenarioAsync(List<ChatScenario> scenarios, Func<ChatHistory, Task<string>> getAnswerFunc)
+    {
+        foreach (var scenario in scenarios)
+        {
+            await CheckChatScenarioAsync(scenario, getAnswerFunc);
+            Log();
+            Log("----------------------------------");
+            Log();
+        }
+    }
+
+
+    /// <summary>
     /// Checks whether the <paramref name="scenario"/> passes on the given <paramref name="kernel"/>
     /// using its ChatCompletionService.
     /// If you want to test the kernel using something other than ChatCompletionService (for example using your own function),
@@ -28,6 +118,7 @@ public partial class SemanticKernelAssert
     /// <param name="getAnswerFunc"></param>
     /// <returns></returns>
     /// <exception cref="InvalidOperationException">If the OpenAI was unable to generate a valid response.</exception>
+    [Obsolete]
     public async Task CheckChatScenarioAsync(Kernel kernel, ChatScenario scenario, Func<ChatHistory, Task<string>>? getAnswerFunc = null)
     {
         var chatHistory = new ChatHistory();
@@ -180,6 +271,7 @@ public partial class SemanticKernelAssert
     /// <param name="getAnswerFunc"></param>
     /// <returns></returns>
     /// <exception cref="InvalidOperationException">If the OpenAI was unable to generate a valid response.</exception>
+    [Obsolete]
     public async Task CheckChatScenarioAsync(Kernel kernel, List<ChatScenario> scenarios, Func<ChatHistory, Task<string>>? getAnswerFunc = null)
     {
         foreach (var scenario in scenarios)
@@ -190,4 +282,7 @@ public partial class SemanticKernelAssert
             Log();
         }
     }
+
+
+
 }
