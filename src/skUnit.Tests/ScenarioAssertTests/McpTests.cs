4601c7274af72cca852df7b67c4395b5cf33f1fc
Mehran Davoudi <mehran.davoudi@hotmail.com>
2025-04-15 23:04:49 +0330

    Add MCP test sample.

---
+++ src/skUnit.Tests/ScenarioAssertTests/McpTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.AI;
+using skUnit.Tests.Infrastructure;
+using System.ComponentModel;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol.Transport;
+using Xunit.Abstractions;
+
+namespace skUnit.Tests.ScenarioAssertTests
+{
+    public class McpTests(ITestOutputHelper output) : SemanticTestBase(output)
+    {
+        [Fact]
+        public async Task AskTime_MustWork()
+        {
+            var clientTransport = new StdioClientTransport(new StdioClientTransportOptions
+            {
+                Name = "Time MCP Server",
+                Command = "cmd",
+                Arguments = [
+                    "/c",
+                    "npx",
+                    "-y",
+                    "@smithery/cli@latest",
+                    "run",
+                    "@yokingma/time-mcp"
+                ],
+            });
+
+            await using var client = await McpClientFactory.CreateAsync(clientTransport);
+
+            var tools = await client.ListToolsAsync();
+
+            var builder = new ChatClientBuilder(BaseChatClient)
+                          .ConfigureOptions(options =>
+                          {
+                              options.Tools ??= [.. tools];
+                          })
+                          .UseFunctionInvocation()
+                ;
+
+            var chatClient = builder.Build();
+
+            var scenarios = await LoadChatScenarioAsync("GetCurrentTimeMcp");
+            await ScenarioAssert.PassAsync(scenarios, chatClient);
+        }
+    }
+
+
+}
---
+++ src/skUnit.Tests/ScenarioAssertTests/Samples/GetCurrentTimeMcp.md
@@ -0,0 +1,17 @@
+# SCENARIO Time Discussion
+
+## [USER]
+What time is it?
+
+## [AGENT]
+10:23
+
+### CHECK SemanticCondition
+It mentions a time.
+
+### CHECK FunctionCall
+```json
+{
+	"function_name": "current_time",
+}
+```
