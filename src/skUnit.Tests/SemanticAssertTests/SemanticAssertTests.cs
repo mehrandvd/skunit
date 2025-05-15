34ba98d2258399b1480ec12bacbd7c1ea8c1dfc0
Mehran Davoudi <mehran.davoudi@hotmail.com>
2025-05-15 18:36:14 +0330

    Add smithery key and env variables.

--- src/skUnit.Tests/SemanticAssertTests/SemanticAssertTests.cs
+++ src/skUnit.Tests/SemanticAssertTests/SemanticAssertTests.cs
@@ -2,42 +2,13 @@ using Azure.AI.OpenAI;
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Configuration;
 using skUnit.Exceptions;
+using skUnit.Tests.Infrastructure;
 using Xunit.Abstractions;
 
 namespace skUnit.Tests.SemanticAssertTests
 {
-    public class SemanticAssertTests
+    public class SemanticAssertTests(ITestOutputHelper output) : SemanticTestBase(output)
     {
-        private ITestOutputHelper Output { get; set; }
-        private SemanticAssert SemanticAssert { get; set; }
-
-        public SemanticAssertTests(ITestOutputHelper output)
-        {
-            Output = output;
-
-            var builder = new ConfigurationBuilder()
-                .AddUserSecrets<SemanticAssertTests>();
-
-            IConfiguration configuration = builder.Build();
-
-            var apiKey =
-                configuration["AzureOpenAI_ApiKey"] ??
-                throw new Exception("No ApiKey is provided.");
-            var endpoint =
-                configuration["AzureOpenAI_Endpoint"] ??
-                throw new Exception("No Endpoint is provided.");
-            var deploymentName =
-                configuration["AzureOpenAI_Deployment"] ??
-                throw new Exception("No Deployment is provided.");
-
-            SemanticAssert = new SemanticAssert(
-                new AzureOpenAIClient(
-                    new Uri(endpoint), 
-                    new System.ClientModel.ApiKeyCredential(apiKey)
-                    ).GetChatClient(deploymentName).AsIChatClient()
-                );
-        }
-
         [Theory]
         [MemberData(nameof(GetSimilarData))]
         public void Similar_True_MustWork(string first, string second)
